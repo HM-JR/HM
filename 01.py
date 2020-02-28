f1010f257843ad23fa40cc82136eef22a4035eed
Tedu <845765697@qq.com>
2020-02-28 10:45:55 +0800

    add jinqu le

---
+++ 01.py
@@ -0,0 +1,18 @@
+'''
+file=open("","","")
+'''
+file=open("3.txt","a+")
+
+# b=file.readline(1)
+# print(b)
+# b=file.readline(15)
+# print(b)
+# b=file.readline(30)
+# print(b)
+file.seek(0,0)
+b=file.readlines()
+b=b[-1]
+b=
+print(b)
+
+file.close()
